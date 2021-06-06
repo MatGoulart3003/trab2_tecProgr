00bb0f4a0453bfb3d9bb063ebf7ad7fd48cc5652
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2021-06-05 22:01:25 -0300

    commitando menu funcionando

--- trab2/src/br/edu/univas/main/StartApp.java
+++ trab2/src/br/edu/univas/main/StartApp.java
@@ -7,15 +7,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import br.edu.univas.view.View;
 import br.edu.univas.vo.*;
 
 public class StartApp {
 	
 	public static void main(String[] args) {
 		
+		Scanner scan              = new Scanner (System.in);
+		int aux;
+		View view                 = new View();
 		FileInputStream file      = null;
-		
+				
 		List<JogoLoto> ListLoto   = new ArrayList<>();
 		List<JogoMega> ListMega   = new ArrayList<>();
 		List<JogoQuina> ListQuina = new ArrayList<>();		
@@ -23,6 +28,15 @@ public class StartApp {
 		ListLoto  =   ReadLoto(file);		
 		ListMega  =   ReadMega(file);
 		ListQuina = ReadQuina (file);
+		
+		do {
+			
+			view.PrintMenuMain();
+			aux = ReadInt(scan);
+			MainMenu(aux,view, scan);
+			
+			
+		}while (aux != 9);
 		
 		
 		
@@ -162,5 +176,71 @@ public class StartApp {
 		return ListJogo;
 		
 	}
+	
+	public static int ReadInt (Scanner scan) {
+		
+		int inteiro = scan.nextInt();
+		scan.nextLine();
+		
+		return inteiro;
+	}
+	
+	public static void MainMenu(int aux, View view, Scanner scan) {
+		
+		int aux2 = aux;
+		
+		if       (aux == 1) {
+				
+			view.PrintMenuSec();	
+			aux = ReadInt(scan);
+			MenuSec(aux, view);
 
-}
+		}else if (aux == 2) {
+			
+			view.PrintMenuSec();
+			aux = ReadInt(scan);
+			MenuSec(aux, view);
+			
+		}else if (aux == 3) {
+			
+			view.PrintMenuSec();
+			aux = ReadInt(scan);
+			MenuSec(aux, view);
+
+		}else if (aux != 9) {
+			
+			view.PrintInvalidOption();
+			aux = aux2;
+		}
+		
+	}
+
+	public static void MenuSec (int aux, View view) {
+		
+		
+		if       (aux == 1) {
+				
+			System.out.println("teste 1");	
+				
+		}else if (aux == 2) {
+				
+			System.out.println("teste 2");	
+				
+		}else if (aux == 3) {
+				
+			System.out.println("teste 3");	
+							
+		}else if (aux == 4) {
+				
+			System.out.println("teste 4");	
+				
+		}else if (aux > 5 || aux < 1) {
+			
+			view.PrintInvalidOption();
+			
+		}
+			
+		
+		
+	}
+} 
--- trab2/src/br/edu/univas/view/View.java
+++ trab2/src/br/edu/univas/view/View.java
@@ -8,15 +8,13 @@ public class View {
 		
 	}
 	
-	
-	
 	public void PrintMenuMain()	{
 		
 		System.out.println("Selecione uma das opções abaixo:");
 		System.out.println("1 - Mega Sena");
 		System.out.println("2 - Quina");
 		System.out.println("3 - Loto-Fácil");
-		System.out.println("9 - Sair\n\n\n");
+		System.out.println("9 - Sair\n\n");
 		
 	}
 	
@@ -26,8 +24,14 @@ public class View {
 		System.out.println("1 - 5 números que mais saíram");
 		System.out.println("2 - 5 números que menos saíram");
 		System.out.println("3 - Gerar números randômicos");
-		System.out.println("4 - Verificar meu jogo\n\n\n");
+		System.out.println("4 - Verificar meu jogo\n\n");
 		
+		
+	}
+	
+	public void PrintInvalidOption () {
+		
+		System.out.println("Opção Inválida!! \nDigite um valor válido\n");
 		
 	}
 	
